126e86053bd6b039b0c35335b5858d6be42dbbf9
yl543 <yl543@duke.edu>
2019-04-03 18:48:44 -0400

    add test cases

--- client.py
+++ client.py
@@ -2,24 +2,29 @@
 
 import socket
 from xml.dom import minidom
-
+import sys
 HOST = '0.0.0.0'  # The server's hostname or IP address
 PORT = 12345        # The port used by the server
 PORT_IN = 23456
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+f=open(sys.argv[1], "r")
+if f.mode == 'r':
+    contents =f.read()
+
 class Connection:
         def send_to_server(self):
-            print('sending')
+          
             s.connect((HOST, PORT))
             #s.sendall(b'<create><symbol sym="AAA"><account id="666">100</account></symbol></create>')
-            s.sendall(b'<transactions id="2"><order sym="AAA" amount="1" limit="103"/></transactions>')
+            s.sendall(bytes(contents, 'utf-8'))
             #data = s.recv(1024)
             #s.sendall(b'<transactions id="10"><order sym="BBB" amount="1000" limit="100"/></transactions>')
         def receive(self):
             xml = ''
-            print('receiving')
+          
             data = s.recv(10240)
-            print(data)
+          
             xml += str(data,'utf-8')
                 #conn.sendall(data)
             print(minidom.parseString(xml).toprettyxml(indent = "    "))
